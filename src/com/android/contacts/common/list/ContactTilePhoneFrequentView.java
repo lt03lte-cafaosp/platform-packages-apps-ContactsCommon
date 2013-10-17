b1bdb7e056d4178eede93c2df002162bb2c4b895
kayiz <kaiyiz@codeaurora.org>
2013-10-17 17:34:07 +0800

    ContactsCommon: Add DSDS feature for frequently call in dialer.
     - remove the background of frequently called view in dialer.
     - move the phone type behind the name.
     - add ssss style and two button style in frequently called view.
    
    CRs-Fixed: 562688
    
    Change-Id: Ifa151959f1a516562cfb8b1196350a82de453237

--- src/com/android/contacts/common/list/ContactTilePhoneFrequentView.java
+++ src/com/android/contacts/common/list/ContactTilePhoneFrequentView.java
@@ -16,13 +16,20 @@
 package com.android.contacts.common.list;
 
 import android.content.Context;
+import android.content.Intent;
+import android.telephony.MSimTelephonyManager;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewStub;
+import android.widget.ImageView;
 
+import com.android.contacts.common.CallUtil;
 import com.android.contacts.common.MoreContactUtils;
+import com.android.contacts.common.R;
 import com.android.contacts.common.list.ContactTileAdapter.ContactEntry;
 import com.android.contacts.common.util.ViewUtil;
+import com.android.internal.telephony.MSimConstants;
 
 /**
  * A dark version of the {@link com.android.contacts.common.list.ContactTileView} that is used in Dialtacts
@@ -33,8 +40,71 @@ import com.android.contacts.common.util.ViewUtil;
 public class ContactTilePhoneFrequentView extends ContactTileView {
     private String mPhoneNumberString;
 
+    private View mSecondaryActionView;
+
+    private View divider_sub1;
+    private View layoutSub1;
+    private ImageView callButtonSub1;
+    private ImageView callIconSub1;
+
+    private View divider_sub2;
+    private View layoutSub2;
+    private ImageView callButtonSub2;
+    private ImageView callIconSub2;
+
     public ContactTilePhoneFrequentView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        ViewStub viewStub = ((ViewStub) findViewById(R.id.secondary_action_stub));
+        mSecondaryActionView = viewStub.inflate();
+        mSecondaryActionView.setVisibility(View.VISIBLE);
+
+        divider_sub1 = mSecondaryActionView.findViewById(R.id.divider_sub1);
+        divider_sub1.setBackgroundResource(R.drawable.ic_divider_dashed_holo_dark);
+        layoutSub1 = mSecondaryActionView.findViewById(R.id.layout_sub1);
+        callButtonSub1 = (ImageView) mSecondaryActionView.findViewById(R.id.call_button_sub1);
+        callButtonSub1.setImageResource(R.drawable.ic_ab_dialer_holo_dark);
+        callIconSub1 = (ImageView) mSecondaryActionView.findViewById(R.id.call_icon_sub1);
+
+        divider_sub2 = mSecondaryActionView.findViewById(R.id.divider_sub2);
+        divider_sub2.setBackgroundResource(R.drawable.ic_divider_dashed_holo_dark);
+        layoutSub2 = mSecondaryActionView.findViewById(R.id.layout_sub2);
+        callButtonSub2 = (ImageView) mSecondaryActionView.findViewById(R.id.call_button_sub2);
+        callButtonSub2.setImageResource(R.drawable.ic_ab_dialer_holo_dark);
+        callIconSub2 = (ImageView) mSecondaryActionView.findViewById(R.id.call_icon_sub2);
+
+        MoreContactUtils.controlCallIconDisplay(mContext, layoutSub1, callButtonSub1, callIconSub1,
+                layoutSub2, callButtonSub2, callIconSub2, divider_sub1, divider_sub2, -1);
+        setSecondaryListener(callButtonSub1, MSimConstants.SUB1);
+        setSecondaryListener(callButtonSub2, MSimConstants.SUB2);
+    }
+
+    private void setSecondaryListener(ImageView imageView, final int subscription) {
+        if (MoreContactUtils.isMultiSimEnable(subscription)) {
+            imageView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (TextUtils.isEmpty(mPhoneNumberString) && mListener != null) {
+                        // Copy "superclass.createClickListener" implementation
+                        mListener.onContactSelected(getLookupUri(), MoreContactUtils
+                                .getTargetRectFromView(mContext,
+                                        ContactTilePhoneFrequentView.this));
+                    } else {
+                        Intent intent = CallUtil.getCallIntent(mPhoneNumberString);
+                        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+                            intent.putExtra(MSimConstants.SUBSCRIPTION_KEY, subscription);
+                            intent.putExtra(MoreContactUtils.DIAL_WIDGET_SWITCHED, subscription);
+                        }
+                        mContext.startActivity(intent);
+                    }
+                }
+            });
+        }
     }
 
     @Override
@@ -59,23 +129,8 @@ public class ContactTilePhoneFrequentView extends ContactTileView {
 
     @Override
     protected OnClickListener createClickListener() {
-        return new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mListener == null) return;
-                if (TextUtils.isEmpty(mPhoneNumberString)) {
-                    // Copy "superclass" implementation
-                    mListener.onContactSelected(getLookupUri(), MoreContactUtils
-                            .getTargetRectFromView(
-                                    mContext, ContactTilePhoneFrequentView.this));
-                } else {
-                    // When you tap a frequently-called contact, you want to
-                    // call them at the number that you usually talk to them
-                    // at (i.e. the one displayed in the UI), regardless of
-                    // whether that's their default number.
-                    mListener.onCallNumberDirectly(mPhoneNumberString);
-                }
-            }
-        };
+        // disable createClickListener, remove "onContactSelected" case to "setSecondaryListener"
+        // method.
+        return null;
     }
 }
